690f12fa08093d71efb5aedf832b5507c7053728
davidjocic93 <davidjocic93@gmail.com>
2017-12-15 13:51:53 +0100

    app finished

---
+++ js/entities.js
@@ -0,0 +1,63 @@
+function Movie(title, length, genre) {
+    this.title = title;
+    this.length = length;
+    this.genre = genre;
+};
+
+Movie.prototype.getInfo = function () {
+    return this.title + ", duration: " + this.length + "min, genre: " + this.genre;
+}
+
+function Program(date) {
+    this.date = date;
+    this.listOfMovies = [];
+    this.totalNumOfMovies = 0;
+};
+
+Program.prototype.getData = function () {
+    return "Date: " + this.date.getDate() + "." + (this.date.getMonth() + 1) + "." + this.date.getFullYear() + ".";
+};
+
+function Festival(name) {
+    this.name = name;
+    this.listOfPrograms = [];
+
+};
+
+Festival.prototype.totalFestMovies = function () {
+    var sum = 0;
+    for (var i = 0; i < this.listOfPrograms.length; i++) {
+        var prog = this.listOfPrograms[i];
+        sum += prog.listOfMovies.length;
+    }
+    return sum;
+};
+
+Festival.prototype.getInfo = function () {
+    var output = '';
+    if (this.listOfPrograms.length < 1) {
+        return output = "Festival program to be announced";
+    }
+    output += "<p>Festival: " + this.name + ", Number of movies: " + this.totalFestMovies() + "</p>";
+    output += "<ol>"
+
+    for (var i = 0; i < this.listOfPrograms.length; i++) {
+        var program = this.listOfPrograms[i];
+        if (program.listOfMovies.length < 1) {
+            output += program.getData();
+            output += "There are no movies on this day.";
+            continue;
+        }
+        output += "<li>" + program.getData() + "</li>";
+
+        output += "<ol>";
+
+        for (var j = 0; j < program.listOfMovies.length; j++) {
+            var movie = program.listOfMovies[j];
+            output += "<li>" + movie.getInfo() + "</li>";
+        }
+        output += "</ol>";
+    }
+    output += "</ol>";
+    return output;
+};
---
+++ js/form.js
@@ -0,0 +1,159 @@
+var movies = [];
+var programs = [];
+var fest = new Festival("Belgrade FEST");
+function createMovie() {
+    var btnAddMov = document.getElementById("btnAddMov");
+    var titleElement = document.getElementById("mTitle");
+    var lengthElement = document.getElementById("mLength");
+    var genreElement = document.getElementById("mGenre");
+    var genreOption = genreElement[genreElement.selectedIndex];
+    var movieList = document.getElementById("movie-list");
+    var errorElement = document.getElementById("movie-error");
+    var moviesSelect = document.getElementById("movies");
+    var btnMovie = document.getElementById('btnMovie');
+
+    var title = titleElement.value.trim();
+    var length = Number(lengthElement.value);
+    var genre = genreOption.value;
+
+    if (!title || !length || length <= 1 || (genre == "none") || typeof length != "number") {
+        if (length <= 0 || typeof length != "number") {
+            errorElement.textContent = "Error! Please enter a valid number!"
+            return;
+        }
+        errorElement.textContent = "Error!"
+        return;
+    }
+
+    for (var mov in movies) {
+        if (title.toLowerCase() == movies[mov].title.toLowerCase()) {
+            errorElement.textContent = "Error!"
+            return;
+        }
+    }
+
+    errorElement.textContent = "";
+    var movie = new Movie(title, length, genre);
+    movies.push(movie);
+    btnAddMov.removeAttribute('disabled');
+
+    var listOfMovies = "<ol>";
+    var moviesOptions = "<option value='none'>--</option>";
+
+    for (var i = 0; i < movies.length; i++) {
+        var film = movies[i];
+        listOfMovies += "<li>" + film.getInfo() + "</li>";
+        moviesOptions += "<option value='" + i + "'>" + film.title + "</option>";
+    }
+    listOfMovies += "</ol>";
+    moviesSelect.innerHTML = moviesOptions;
+    movieList.innerHTML = listOfMovies;
+};
+
+function createProgram() {
+    var btnAddMov = document.getElementById("btnAddMov");
+    var dateElement = document.getElementById("pDate").value;
+    var programList = document.getElementById("program-list");
+    var errorElement = document.getElementById("prog-error");
+    var programsSelect = document.getElementById("programs");
+    var programsFestSelect = document.getElementById("programFest");
+
+    if (!dateElement) {
+        errorElement.textContent = "Error!"
+        return;
+    }
+
+    for (var prog in programs) {
+        if ((new Date(dateElement)).toDateString() == programs[prog].date.toDateString()) {
+            errorElement.textContent = "Error! Program already created."
+            return;
+        }
+    }
+    errorElement.textContent = "";
+
+    var program = new Program(new Date(dateElement));
+    programs.push(program);
+    btnAddMov.removeAttribute('disabled');
+
+    var listOfPrograms = "<ol>";
+    var programsOptions = "<option value='none'>--</option>";
+
+    for (var i = 0; i < programs.length; i++) {
+        var prog = programs[i];
+        listOfPrograms += "<li>" + prog.getData() + "</li>";
+        programsOptions += "<option value='" + i + "'>" + prog.getData() + "</option>";
+    }
+    listOfPrograms += "</ol>";
+
+    programList.innerHTML = listOfPrograms;
+    programsSelect.innerHTML = programsOptions;
+    programsFestSelect.innerHTML = programsOptions;
+};
+
+function addMovie() {
+    var programList = document.getElementById("progmov-list");
+    var selectMovieElement = document.getElementById("movies");
+    var selectMovieOption = selectMovieElement[selectMovieElement.selectedIndex].value;
+    var selectProgramElement = document.getElementById("programs");
+    var selectProgramOption = selectProgramElement[selectProgramElement.selectedIndex].value;
+    var errorElement = document.getElementById("progmov-error");
+
+    if (selectProgramOption == "none" || selectMovieOption == "none") {
+        errorElement.textContent = "Error!"
+        return;
+    }
+    errorElement.textContent = "";
+
+    for (var i = 0; i < programs[selectProgramOption].listOfMovies.length; i++) {
+        if (movies[selectMovieOption] == programs[selectProgramOption].listOfMovies[i]) {
+            errorElement.textContent = "Error! Movie already added to the selected program."
+            return;
+        }
+    }
+    errorElement.textContent = "";
+
+    programs[selectProgramOption].listOfMovies.push(movies[selectMovieOption]);
+
+    var listOfMoviesInProgram = programs[selectProgramOption].listOfMovies;
+    var programOutput = "<p>" + programs[selectProgramOption].getData() + "</p>";
+    programOutput += "<ol>"
+
+    for (var i = 0; i < listOfMoviesInProgram.length; i++) {
+        var movieInProgram = listOfMoviesInProgram[i];
+        programOutput += "<li>" + movieInProgram.getInfo() + "</li>";
+    }
+    programOutput += "</ol>"
+
+    programList.innerHTML = programOutput;
+
+};
+function addProgram() {
+    var selectProgramElement = document.getElementById("programFest");
+    var selectProgramOption = selectProgramElement[selectProgramElement.selectedIndex].value;
+    var errorElement = document.getElementById("progfest-error");
+    errorElement.textContent = "";
+    if (selectProgramOption == "none") {
+        errorElement.textContent = "Error!"
+        return;
+    }
+    errorElement.textContent = "";
+
+    for (var i = 0; i < fest.listOfPrograms.length; i++) {
+        if (fest.listOfPrograms[i] == programs[selectProgramOption]) {
+            errorElement.textContent = "Error! Program already added to the festival."
+            return;
+        }
+    }
+    errorElement.textContent = "";
+
+    fest.listOfPrograms.push(programs[selectProgramOption]);
+    errorElement.textContent = "Program added to festival!"
+
+};
+function festivalProgram() {
+    var progFestElement = document.getElementById("progfest-list");
+
+    var output = fest.getInfo();
+    progFestElement.innerHTML = output;
+
+}
